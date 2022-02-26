5454851ee70360af80e896e1e4f44f20f889d42d
Diego Escobar <diego.netsolutions@gmail.com>
2022-02-26 20:36:19 +0200

    updated cors and logic of business

--- public/index.php
+++ public/index.php
@@ -1,20 +1,50 @@
 <?php
 
-require_once __DIR__ . '/../helpers/router.php';
 
-// With named parameters
-router('GET', '^/$', function($params) {
-    echo "ONLINE Server";
-    var_dump($params);
-});
+try {
 
-// POST request to /api
-router('POST', '^/api$', function() {
-    header('Content-Type: application/json');
-    $json = json_decode(file_get_contents('php://input'), true);
-    var_dump($json["test1"]);
-    echo json_encode($json);
-});
+    require_once __DIR__ . '/../helpers/router.php';
+    require_once __DIR__ . '/../helpers/cors.php';
+    require_once __DIR__ . '/../helpers/get_user_local_time.php';
+    require_once __DIR__ . '/../services/insuranceServices.php';
 
-header("HTTP/1.0 404 Not Found");
-echo '404 Not Found';
+
+
+    cors();
+
+    // With named parameters
+    router('GET', '^/$', function () {
+        echo "Server ONLINE";
+    });
+
+    // POST request to /api
+    router('POST', '^/api$', function () {
+        header('Content-Type: application/json');
+        $json = json_decode(file_get_contents('php://input'), true);
+        $car_price = $json['car_price'];
+        $tax_percentage = $json['tax_percentage'];
+        $number_of_policies = $json['number_of_policies'];
+        $local_time = $json['local_time'];
+
+        $local_time_formated = date_create($local_time);
+        // Check if any of the parameters are empty
+        if (is_null($car_price) || is_null($tax_percentage) || is_null($number_of_policies) || is_null($local_time)) {
+            echo json_encode(['error' => 'Missing parameters']);
+            return;
+        }
+
+
+        $week_day = date('w', $local_time_formated->getTimestamp());
+        $hour_of_day = date('H', $local_time_formated->getTimestamp());
+
+        echo json_encode([
+            'user_ip_hour' => get_user_local_time(),
+            'insurance_data' => calculate_insurance($car_price, $tax_percentage, $number_of_policies, $week_day, $hour_of_day)
+        ]);
+    });
+
+    header("HTTP/1.0 404 Not Found");
+    echo '404 Not Found';
+} catch (\Throwable $th) {
+    echo json_encode(['error' => $th->getMessage()]);
+}
--- services/insuranceServices.php
+++ services/insuranceServices.php
@@ -1,7 +1,48 @@
 <?php
 
-function calculateInsurance($data)
-{
-return $data;
 
+
+function get_base_price_policy($week_day, $hour_of_day)
+{
+    return ($week_day == 0 && $hour_of_day >= 15 && $hour_of_day < 20)
+        ? 13 : 11;
+};
+
+function get_cost_data($car_price, $tax_percentage, $base_price_policy, $is_base_policy = false)
+{
+
+    $COMISION_TAX = 17;
+
+    $base_premium = $car_price + ($car_price * $base_price_policy / 100);
+
+    $commision = $base_premium * $COMISION_TAX / 100;
+
+    $tax = $base_premium * $tax_percentage / 100;
+
+    return [
+        'base_premium' => $is_base_policy ? $base_premium : "",
+        'commision' => $commision,
+        'tax' => $tax,
+        'total_cost' => $base_premium + $commision + $tax
+
+    ];
+};
+
+function calculate_insurance($car_price, $tax_percentage, $number_of_policies, $week_day, $hour_of_day)
+{
+
+    $base_price_policy = get_base_price_policy($week_day, $hour_of_day);
+
+    $output_data = [
+        'policy' => get_cost_data($car_price, $tax_percentage, $base_price_policy, true),
+    ];
+
+    $policy_price = $car_price / $number_of_policies;
+
+    for ($i = 1; $i < $number_of_policies; $i++) {
+
+        $output_data[$i . '_instalment'] = get_cost_data($policy_price, $tax_percentage, $base_price_policy);
+    }
+
+    return $output_data;
 }
