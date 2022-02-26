7c701b4e5b4bb03757776606a769535804102180
Diego Escobar <diego.netsolutions@gmail.com>
2022-02-26 22:11:47 +0200

    fix to see more info

--- services/get_user_local_time.php
+++ services/get_user_local_time.php
@@ -7,13 +7,15 @@ function get_user_local_time()
     $user_ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR']);
 
     if (!filter_var($user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-        return "INVALID USER IP " . $user_ip;
+        //return "INVALID USER IP " . $user_ip;
+        $user_ip = "213.35.190.234";
     }
 
     $url = "http://ipinfo.io/" . $user_ip;
     $ip_info = json_decode(file_get_contents($url));
+    echo json_encode($ip_info);
 
-    $timezone = $ip_info->timezone;
+    $timezone = "dssadasf"; //$ip_info->timezone;
     $date = new DateTime(date('m/d/Y h:i:s a', time()));
     $date->setTimezone(new DateTimeZone($timezone));
 
